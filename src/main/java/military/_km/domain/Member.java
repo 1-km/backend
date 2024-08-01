32d4a7e5599b80195fc0e44490b3ac348d0b3313
mirikwon427 <mirikwon2719@gmail.com>
2024-08-01 12:15:20 +0900

    fix : member 도메인에 socialCode 추가

--- src/main/java/military/_km/domain/Member.java
+++ src/main/java/military/_km/domain/Member.java
@@ -1,6 +1,7 @@
 package military._km.domain;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import jakarta.persistence.*;
 import lombok.*;
 import military._km.converter.MilitaryConverter;
@@ -13,41 +14,48 @@ import java.util.Date;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
 @AllArgsConstructor
-public class Member extends BaseTimeEntity{
+public class Member extends BaseTimeEntity {
 
-    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "member_id")
-    private Long id;
-    @Column(nullable = false,name = "member_email")
-    private String email;
-    @JsonIgnore
-    @Column(nullable = false, name = "member_password")
-    private String password;
-    @Column(nullable = false, name = "member_nickname")
-    private String nickname;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "member_id")
+	private Long id;
 
-    @Convert(converter = MilitaryConverter.class)
-    @Column(name="member_military")
-    private Military military;
-
-    @Enumerated(EnumType.STRING)
-    @Column(name = "member_role")
-    private Role role;
-
-    @Column(name = "member_base")
-    private String base;
-
-    @Column(name = "member_startdate")
-    private String startdate;
-
-    @Column(name = "member_finishdate")
-    private String finishdate;
+	@Column(nullable = false, name = "member_email")
+	private String email;
 
     /**
-     * 소셜 로그인 사용자의 경우 이 필드에는 제공자 ID(예: "google", "naver", "kakao")가 저장
-     * 이메일 인증 사용자의 경우 이 필드는 null
+     * oAuth2 로그인 경우 비밀번호 필드는 null
      */
-    @Column(name = "socialCode")
-    private SocialCode socialCode;
+	@JsonIgnore
+	@Column(name = "member_password")
+	private String password;
+
+	@Column(nullable = false, name = "member_nickname")
+	private String nickname;
+
+	@Convert(converter = MilitaryConverter.class)
+	@Column(name = "member_military")
+	private Military military;
+
+	@Enumerated(EnumType.STRING)
+	@Column(name = "member_role")
+	private Role role;
+
+	@Column(name = "member_base")
+	private String base;
+
+	@Column(name = "member_startdate")
+	private String startdate;
+
+	@Column(name = "member_finishdate")
+	private String finishdate;
+
+	/**
+	 * 소셜 로그인 사용자의 경우 이 필드에는 제공자 ID(예: "google", "naver", "kakao")가 저장
+	 * 이메일 인증 사용자의 경우 이 필드에는 제공자 ID("email")가 저장
+	 */
+	@Column(name = "socialCode")
+	private SocialCode socialCode;
 
 }
